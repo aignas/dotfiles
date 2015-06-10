1c0503f1e98127e5b16f0ccbc3552a552829466b
Ignas Anikevicius <anikevicius@gmail.com>
2015-06-10 22:12:26 +0100

    Vim: use NeoBundle and other Shougo plugins.

--- vim/install.sh
+++ vim/install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 mkdir -p ~/.vim/bundle
-TARGET_DIR=~/.vim/bundle/Vundle.vim
 
-if [ -d ${TARGET_DIR} ]; then
-    cd ${TARGET_DIR}
-    git pull
-    cd -
-else
-    git clone https://github.com/gmarik/Vundle.vim.git ${TARGET_DIR}
-fi
+echo "Create backup, swp and undo directories"
+mkdir -p ${HOME}/.vim/{backup,swp,undo}
