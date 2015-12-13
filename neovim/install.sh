c30607b5a91bdfcd47404d5c638bf60be06b97bb
Ignas Anikevicius <anikevicius@gmail.com>
2015-12-13 22:53:03 +0000

    Major updates to neovim: use a different plugin manager, use neomake instead of syntastic

--- neovim/install.sh
+++ neovim/install.sh
@@ -2,10 +2,11 @@
 
 NEOVIM_LOCAL=${HOME}/.local/nvim
 
-echo "Install NeoBundle"
-mkdir -p ${NEOVIM_LOCAL}/bundle
-NEOBUNDLE_REPO="https://github.com/Shougo/neobundle.vim"
-git clone ${NEOBUNDLE_REPO} ${NEOVIM_LOCAL}/bundle/neobundle.vim
+echo "Install Vim-plug"
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p ${NEOVIM_LOCAL}/plugged
 
 echo "Create backup, swp and undo directories"
 mkdir -p ${NEOVIM_LOCAL}/{backup,swp,undo}
