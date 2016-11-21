5f7e2f537ee00500aec49407595ccc139db7971d
Ignas Anikevicius <anikevicius@gmail.com>
2016-11-21 21:56:35 +0200

    Add a function to setup a python test env

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -52,3 +52,29 @@ if [[ ${DOTFILES_OS} != "Darwin" ]]; then
         done
     }
 fi
+
+# A function for setting up python dev/test environment for new projects:
+function pyvenv_setup() {
+    local bootstrap=py-bootstrap.sh
+    cat > $bootstrap << EOF
+#!/bin/bash
+
+# Setup a virtualenv for Python 3.0
+pyvenv venv
+venv/bin/pip install --upgrade \
+    pip \
+    setuptools \
+    pylint \
+    tox \
+    pytest
+EOF
+
+    chmod 755 $bootstrap
+    ./$bootstrap
+
+    # Generate pylint RC file
+    venv/bin/pylint --generate-rcfile >> .pylintrc
+
+    # Create a tests directory
+    mkdir -p tests
+}
