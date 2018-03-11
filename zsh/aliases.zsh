39f417fccf7e427493bb8af575c53d9dc6c6dfbc
Ignas Anikevicius <anikevicius@gmail.com>
2018-03-11 19:47:07 +0200

    Zsh updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,30 +5,18 @@ DOTFILES_OS=`uname -s`
 
 # Normal aliases
 alias visudo='sudo -E EDITOR=nvim visudo'
-#alias ls='ls --color=auto -FX'
 alias se='sudo -E'
-alias tm='tmux'
-alias tmn='tmux neww'
-alias gexp='git archive master | tar -x -C'
-alias vimconfig='vim ~/.vimrc'
 alias nvimconfig='nvim ~/.config/nvim/init.vim'
 alias g="git"
 alias push-notes="rclone sync ~/Dropbox/Notes db:Notes"
 alias pull-notes="rclone sync db:Notes ~/Dropbox/Notes"
-
 alias todo="$EDITOR +VimFiler \"$HOME/Dropbox (Personal)/_GTD/TODO.md\""
 alias worklog="$EDITOR +VimFiler \"$HOME/Dropbox (TRAFI)/Notes/\""
-
-if [[ ${DOTFILES_OS} == "Darwin" ]]; then
-    alias ls='ls -G'
-else
-    alias ls='ls --color=auto'
-fi
-
+alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias less='less -R'
-
 alias ranger='if [ -z "$RANGER_LEVEL" ]; then ranger; else exit; fi'
+
 # Have only one fzf-fs session running
 alias f='if [[ -z ${FZF_FS_DEFAULT_OPTS_OLD+x} ]]; then fzf-fs; else exit; fi'
 
