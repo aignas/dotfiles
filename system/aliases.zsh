29eb51e73bd199defada70e3ab87661e8c2b4acd
Ignas Anikevicius <anikevicius@gmail.com>
2018-07-10 23:14:28 +0300

    Chmod and more cleanup.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,27 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+if [[ ${ZSH_DOTFILES_OS} == "ArchLinux" ]]; then
+    PKGBUILD_DIR="${HOME}/src/aur"
+
+    function aurd () {
+        mkdir -p ${PKGBUILD_DIR}
+        for pkg in $@; do
+            git clone https://aur.archlinux.org/$pkg.git/ ${PKGBUILD_DIR}/$pkg
+        done
+    }
+
+    function aurb () {
+        mkdir -p ${PKGBUILD_DIR}
+        # Download everything first
+        aurd $@
+
+        for pkg in $@; do
+            pushd ${PKGBUILD_DIR}/$pkg
+            makepkg -si
+            popd # ${PKGBUILD_DIR}/$pkg
+        done
+    }
+fi
+
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,44 +4,19 @@ alias reload!='. ~/.zshrc'
 DOTFILES_OS=`uname -s`
 
 # Normal aliases
-alias visudo='sudo -E EDITOR=nvim visudo'
+alias visudo='sudo -E EDITOR=$EDITOR visudo'
 alias se='sudo -E'
-alias nvimconfig='nvim ~/.config/nvim/init.vim'
+alias vc='$EDITOR ~/.config/nvim/init.vim'
 alias g="git"
 alias push-notes="rclone sync ~/Dropbox/Notes db:Notes"
 alias pull-notes="rclone sync db:Notes ~/Dropbox/Notes"
 alias todo="$EDITOR +VimFiler \"$HOME/Dropbox (Personal)/_GTD/TODO.md\""
 alias worklog="$EDITOR +VimFiler \"$HOME/Dropbox (TRAFI)/Notes/\""
 alias ls='ls --color=auto'
+alias ll='ls -l --color=auto'
 alias grep='grep --color=auto'
 alias less='less -R'
 alias ranger='if [ -z "$RANGER_LEVEL" ]; then ranger; else exit; fi'
-
-# Have only one fzf-fs session running
-alias f='if [[ -z ${FZF_FS_DEFAULT_OPTS_OLD+x} ]]; then fzf-fs; else exit; fi'
-
-if [[ ${DOTFILES_OS} != "Darwin" ]]; then
-    PKGBUILD_DIR="${HOME}/src/aur"
-
-    function aurd () {
-        mkdir -p ${PKGBUILD_DIR}
-        for pkg in $@; do
-            git clone https://aur.archlinux.org/$pkg.git/ ${PKGBUILD_DIR}/$pkg
-        done
-    }
-
-    function aurb () {
-        mkdir -p ${PKGBUILD_DIR}
-        # Download everything first
-        aurd $@
-
-        for pkg in $@; do
-            pushd ${PKGBUILD_DIR}/$pkg
-            makepkg -si
-            popd # ${PKGBUILD_DIR}/$pkg
-        done
-    }
-fi
 
 # A function for setting up python dev/test environment for new projects:
 function pyvenv_setup() {
