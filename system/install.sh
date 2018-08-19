8f1a9c8b1e8f99c98313383ebc7f8c90c3baaee5
Ignas Anikevicius <anikevicius@gmail.com>
2018-08-19 21:44:13 +0300

    Add ripgrep to the installation

--- system/install.sh
+++ system/install.sh
@@ -1,7 +1,9 @@
 if [[ ${ZSH_DOTFILES_OS} == "Darwin" ]]; then
-    brew install httpie fd exa
+    brew install httpie fd exa ripgrep
 elif [[ ${ZSH_DOTFILES_OS} == "ArchLinux" ]]; then
-    sudo pacman -Sy --needed httpie python neovim ranger fd exa
+    sudo pacman -Sy --needed httpie python neovim ranger fd exa ripgrep
 elif [[ ${ZSH_DOTFILES_OS} == "Debian" ]]; then
     sudo apt-get install httpie ranger
+    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
+    sudo dpkg -i ripgrep_0.8.1_amd64.deb
 fi
