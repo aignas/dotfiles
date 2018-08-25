d50a52181b45cc5d0d1513dfad65062b8848af45
Ignas Anikevicius <aignas@uber.com>
2018-08-25 18:07:52 +0300

    Fix a few install scripts

--- system/install.sh
+++ system/install.sh
@@ -1,7 +1,7 @@
-if [[ ${ZSH_DOTFILES_OS} == "Darwin" ]]; then
-    brew install httpie fd exa ripgrep
+if [[ ${ZSH_DOTFILES_OS} == "Mac" ]]; then
+    brew install python3 httpie fd exa ripgrep htop ranger task timewarrior
 elif [[ ${ZSH_DOTFILES_OS} == "ArchLinux" ]]; then
-    sudo pacman -Sy --needed httpie python neovim ranger fd exa ripgrep
+    sudo pacman -Sy --needed httpie python neovim ranger fd exa ripgrep htop task timewarrior
 elif [[ ${ZSH_DOTFILES_OS} == "Debian" ]]; then
     sudo apt-get install httpie ranger
     curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
--- tmux/install.sh
+++ tmux/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+DIR=$HOME/.tmux/plugins/tpm
+
+if [ ! -d $DIR ]; then
+    git clone https://github.com/tmux-plugins/tpm $DIR
+fi
