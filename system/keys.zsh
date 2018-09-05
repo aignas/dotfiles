9116535c3661125cf4f949345d795a142e26a1a7
Ignas Anikevicius <aignas@uber.com>
2018-09-05 09:56:14 +0300

    Fix pubkey

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,6 @@
 # Pipe my public key to my clipboard.
-alias pubkey="xclip -sel clip < ~/.ssh/id_rsa.pub | echo '=> Public key copied to the clipboard.'"
+if [[ ${ZSH_DOTFILES_OS} == "Mac" ]]; then
+    alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+else
+    alias pubkey="xclip -sel clip < ~/.ssh/id_rsa.pub | echo '=> Public key copied to the clipboard.'"
+fi
