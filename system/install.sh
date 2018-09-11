58ed05a71cd96d7ceb5c69dae58f502e1523cd51
Ignas Anikevicius <anikevicius@gmail.com>
2018-09-11 23:47:03 +0300

    Add some system packages for Arch

--- system/install.sh
+++ system/install.sh
@@ -1,7 +1,7 @@
 if [[ ${ZSH_DOTFILES_OS} == "Mac" ]]; then
     brew install python3 httpie fd exa ripgrep htop ranger task timewarrior
 elif [[ ${ZSH_DOTFILES_OS} == "ArchLinux" ]]; then
-    sudo pacman -Sy --needed httpie python neovim ranger fd exa ripgrep htop task timewarrior
+    sudo pacman -Sy --needed httpie python neovim ranger fd exa ripgrep htop task timew dep
 elif [[ ${ZSH_DOTFILES_OS} == "Debian" ]]; then
     sudo apt-get install httpie ranger
     curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
