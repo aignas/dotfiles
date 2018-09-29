cb970815e06549bfdca63de0612f54d35e45acbb
Ignas Anikevicius <anikevicius@gmail.com>
2018-09-29 12:54:26 +0300

    Install bat

--- system/install.sh
+++ system/install.sh
@@ -1,8 +1,8 @@
 if [[ ${ZSH_DOTFILES_OS} == "Mac" ]]; then
-    brew install python3 httpie fd exa ripgrep htop ranger task timewarrior
+    brew install python3 httpie fd exa ripgrep htop ranger bat
     brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 elif [[ ${ZSH_DOTFILES_OS} == "ArchLinux" ]]; then
-    sudo pacman -Sy --needed httpie python neovim ranger fd exa ripgrep htop task timew dep
+    sudo pacman -Sy --needed httpie python neovim ranger fd exa ripgrep bat htop dep
 elif [[ ${ZSH_DOTFILES_OS} == "Debian" ]]; then
     sudo apt-get install httpie ranger
     curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
