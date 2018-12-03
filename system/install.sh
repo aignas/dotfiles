3a88956bdf5404a21f4278fa1d8199b059012f10
Ignas Anikevicius <anikevicius@gmail.com>
2018-12-03 06:53:10 +0200

    Update system installation scripts

--- system/install.sh
+++ system/install.sh
@@ -1,10 +1,15 @@
 if [[ ${ZSH_DOTFILES_OS} == "Mac" ]]; then
     brew install python3 httpie fd exa ripgrep htop ranger bat
-    brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+    brew cask install alacritty
 elif [[ ${ZSH_DOTFILES_OS} == "ArchLinux" ]]; then
-    sudo pacman -Sy --needed httpie python neovim ranger fd exa ripgrep bat htop dep
-elif [[ ${ZSH_DOTFILES_OS} == "Debian" ]]; then
-    sudo apt-get install httpie ranger
-    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
-    sudo dpkg -i ripgrep_0.8.1_amd64.deb
+    sudo pacman -Sy --needed \
+        httpie \
+        python \
+        neovim \
+        ranger \
+        fd exa ripgrep bat \
+        alacritty \
+        htop \
+        dep \
+        skk-jisyo
 fi
