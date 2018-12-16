9626aef2e7b6954162c6b9d5db5fd1dd5965c8b2
Ignas Anikevicius <anikevicius@gmail.com>
2018-12-16 20:41:23 +0200

    Instead of Makefile, use a simple bash script

--- sh-plugins/install.sh
+++ sh-plugins/install.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-make bin
+readonly local plugins=git_prompt
+readonly local bin_dir="${DOTFILES}/sh-plugins/bin"
+
+mkdir -p "${bin_dir}"
+
+for i in ${plugins}
+do
+  echo "=== Building $i ==="
+  pushd "${DOTFILES}/sh-plugins/$i" || exit 1
+  cargo -Z unstable-options build --release --out-dir "${bin_dir}"
+  popd || exit 1
+done
