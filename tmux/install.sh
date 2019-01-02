e50bf499a3a1b9546d1d871bbad5157323ec9faf
Ignas Anikevicius <aignas@uber.com>
2019-01-02 16:55:48 +0200

    Use TPM to update the plugins when installing everything

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 DIR=$HOME/.tmux/plugins/tpm
-[[ -d "$DIR" ]] || \
+if [[ -d "$DIR" ]]
+then
+  git -C "${DIR}" fetch origin/master
+  git -C "${DIR}" reset --hard origin/master
+else
   git clone \
     --depth=1 \
     --branch=master \
     https://github.com/tmux-plugins/tpm "$DIR"
+fi
+
+"$DIR/bin/clean_plugins"
+"$DIR/bin/update_plugins" all
+"$DIR/bin/install_plugins"
