76805d98a83f91145d966832f77c377b8c211802
Ignas Anikevicius <aignas@uber.com>
2019-02-15 13:11:50 +0200

    Add entr tool

--- system/install.sh
+++ system/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-readonly common=(httpie fd exa ripgrep bat htop neovim ranger zsh hub nnn tig)
+readonly common=(httpie fd exa ripgrep bat htop neovim ranger zsh hub nnn tig entr)
 cd "$(dirname "$0")/.."
 # shellcheck source=/dev/null
 source script/logging.sh
