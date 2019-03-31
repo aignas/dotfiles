134955794439d40aa526069b4b598b45a4762053
Ignas Anikevicius <aignas@uber.com>
2019-03-31 16:58:09 +0300

    Add a script to download a lithuanian layout

---
+++ keyboard/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+[[ ${DOTFILES_OS} != "Mac" ]] && exit 0
+
+readonly filename=MacOS-lt-std-keyboard-layout.zip
+readonly dest="${HOME}/Library/Keyboard Layouts"
+
+curl http://www.ims.mii.lt/klav/MacOS-X.zip > ${filename}
+trap 'rm ${filename}' EXIT
+
+# Unpack into target
+unzip -o ${filename} -d "${dest}"
+trap - EXIT
+rm ${filename}
