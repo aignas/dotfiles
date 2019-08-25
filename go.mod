ccea995b7af1340efc487e341eb6268b418f476d
Ignas Anikevicius <anikevicius@gmail.com>
2019-08-25 12:22:08 +0300

    go: add extra cmd tools and remove shfmt

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.12
 
 require (
 	github.com/golangci/golangci-lint v1.17.1 // indirect
+	golang.org/x/tools v0.0.0-20190825031127-d72b05d2b1b6 // indirect
 	golang.org/x/tools/gopls v0.1.3 // indirect
-	mvdan.cc/sh v2.6.4+incompatible // indirect
 )
--- golang/install.sh
+++ golang/install.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# For differences between the language servers one can visit: https://fferences between go-langserver, bingo, golsp
-# Copy below:
-#
-# - [go-langserver](https://github.com/sourcegraph/go-langserver)
-#
-# > go-langserver is designed for online code reading such as github.com.
-#
-# - [bingo](https://github.com/saibing/bingo)
-#
-# > bingo is designed for offline editors such as vscode, vim, it focuses on
-# code editing.
-#
-# - [gopls](https://github.com/golang/go/wiki/gopls)
-#
-# > gopls is an official language server,  and it is currently in early development.
-#
-
 export GO111MODULE=on
 go get golang.org/x/tools/gopls@latest
+go get golang.org/x/tools/cmd/stringer@latest
+go get golang.org/x/tools/cmd/gorename@latest
 go get github.com/golangci/golangci-lint/cmd/golangci-lint@latest
-go get mvdan.cc/sh/cmd/shfmt@latest
