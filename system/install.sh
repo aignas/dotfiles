38b806fa897562b2361ac95995564e5570766b6b
Ignas Anikevicius <anikevicius@gmail.com>
2019-08-25 12:21:51 +0300

    Install shfmt via pkg managers

--- system/install.sh
+++ system/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-readonly common=(fd exa ripgrep bat htop neovim ranger zsh nnn tig tmux watchexec bazel)
+readonly common=(fd exa ripgrep bat htop neovim ranger zsh nnn tig tmux watchexec bazel shfmt)
 cd "$(dirname "$0")/.."
 # shellcheck source=/dev/null
 source script/logging.sh
