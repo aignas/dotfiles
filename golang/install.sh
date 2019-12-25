508edfdb8d0a22a41dabeda58c6f8067ac9456da
Ignas Anikevicius <anikevicius@gmail.com>
2019-12-26 00:03:11 +0900

    conditional golang toolchain setup

--- golang/install.sh
+++ golang/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-export GO111MODULE=on
-go get golang.org/x/tools/gopls@latest
-go get golang.org/x/tools/cmd/stringer@latest
-go get golang.org/x/tools/cmd/gorename@latest
-go get github.com/golangci/golangci-lint/cmd/golangci-lint@latest
-go get github.com/sourcegraph/src-cli/cmd/src@latest
+if [[ -f "$(command -v go)" ]]; then
+    export GO111MODULE=on
+    go get golang.org/x/tools/gopls@latest
+    go get golang.org/x/tools/cmd/stringer@latest
+    go get golang.org/x/tools/cmd/gorename@latest
+    go get github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+    go get github.com/sourcegraph/src-cli/cmd/src@latest
+fi
