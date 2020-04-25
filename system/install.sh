19cfd6a259f2280678f3dd32d84e50a3b0241268
Ignas Anikevicius <anikevicius@gmail.com>
2020-04-25 12:43:13 +0300

    remove ArchLinux specific setup

--- system/install.sh
+++ system/install.sh
@@ -8,45 +8,18 @@ source script/logging.sh
 DOTFILES_OS="${DOTFILES_OS:-"$(get_os)"}"
 
 echo "The detected OS is ${DOTFILES_OS}"
-if [[ ${DOTFILES_OS} == "Mac" ]]; then
-    info "brew"
-    brew update
-    ok "brew update"
-    brew cleanup
-    ok "brew cleanup"
-    brew upgrade
-    ok "brew upgrade"
-    brew cask upgrade
-    ok "brew cask upgrade"
-
-    info "Installing packages"
-    bottles=(jq fd exa ripgrep bat htop neovim zsh tmux watchexec shfmt python3 coreutils gnu-sed skktools)
-    brew install "${bottles[@]}" || :
-
-    info "Installing packages via pip"
-    pip3 install --upgrade -U vim-vint
-elif [[ ${DOTFILES_OS} == "ArchLinux" ]]; then
-    info "pacman"
-    debug "pacman: update"
-    sudo pacman -Syu --noconfirm
-    ok "pacman: update"
-
-    debug "pacman: install"
-    pkgs=(jq python base-devel fd exa ripgrep bat htop neovim zsh tmux watchexec shfmt shellcheck skk-jisyo pamixer otf-fira-code rustup go vint)
-    sudo pacman -S \
-        --quiet \
-        --needed \
-        --noconfirm \
-        "${pkgs[@]}"
-    ok "pacman: install"
-elif [[ ${DOTFILES_OS} == "Debian" ]]; then
+if [[ ${DOTFILES_OS} == "Debian" ]]; then
     info "apt-get"
     sudo apt-get update
     ok "apt-get: update"
 
     debug "apt-get: install"
     sudo apt-get install -y \
-        jq python3 python3-venv fd-find exa ripgrep htop neovim zsh tmux shellcheck curl unzip
+        jq python3 python3-venv fd-find exa ripgrep htop neovim zsh tmux \
+        shellcheck curl unzip
 
     ok "apt-get: install"
+else
+    info "Unknown OS"
+    ok "Unknown OS: noop"
 fi
