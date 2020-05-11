a18be8a4c3fde3fc9f396c108fea0a65d99784b2
Ignas Anikevicius <anikevicius@gmail.com>
2020-05-11 09:21:25 +0300

    do not fail installation of hooks when already installed

--- git/install.sh
+++ git/install.sh
@@ -5,4 +5,4 @@ readonly _gh=${DOTFILES}/tools/git-hooks
 echo "UPDATING git-hooks"
 curl -o "$_gh" https://raw.githubusercontent.com/icefox/git-hooks/master/git-hooks
 chmod +x "$_gh"
-"$_gh" --install
+"$_gh" --install || :
