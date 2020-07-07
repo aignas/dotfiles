0ab190e7371806901cb5fd2e96ab5e4220079683
Ignas Anikevicius <aignas@uber.com>
2020-07-07 09:11:00 +0300

    changes to go development environment

--- golang/tools.go
+++ golang/tools.go
@@ -5,6 +5,7 @@ package tools
 import (
 	_ "github.com/gokcehan/lf"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "golang.org/x/tools/cmd/godoc"
 	_ "golang.org/x/tools/cmd/gorename"
 	_ "golang.org/x/tools/cmd/stringer"
 	_ "golang.org/x/tools/gopls"
