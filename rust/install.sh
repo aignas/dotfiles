5bb94073fae3dcc46100b5eec24c7cd4ae541270
Ignas Anikevicius <anikevicius@gmail.com>
2020-12-30 18:18:21 +0200

    fix the installation of the rs-git-fsmonitor

--- rust/install.sh
+++ rust/install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+set -e
 
 if [[ ! -f "$(command -v rustup)" ]]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
 rustup toolchain install stable beta
-rustup component add clippy rustfmt rls
+rustup component add clippy rustfmt \
+    rust-src # needed for rust-analyzer
 rustup default stable
 rustup update
 
 cargo install watchexec
+cargo install --git=https://github.com/jgavris/rs-git-fsmonitor.git
 
-curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux -o tools/rust-analyzer
-chmod +x tools/rust-analyzer
+download() {
+    local -r url="https://github.com/${1}/releases/latest/download/${2}"
+    local -r dest="tools/${3:-$2}"
+
+    curl -L "$url" -o "$dest" && chmod +x "$dest"
+}
+
+download rust-analyzer/rust-analyzer rust-analyzer-linux rust-analyzer
