bb8a546fc7f90f5e0993d0aa495ba8b199522f89
Ignas Anikevicius <anikevicius@gmail.com>
2021-05-05 22:05:14 +0300

    neovim: simplify install script

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,34 +1,8 @@
 #!/bin/bash
-
-set -e
-
-cd "$(dirname "$0")/.."
-# shellcheck source=/dev/null
-. script/logging.sh
+set -eu
 
 XDG_DATA_HOME="${XDG_DATA_HOME:-"${HOME}"/.local/share}"
+mkdir -p "${XDG_DATA_HOME}"/nvim/{backup,skk}
 
-backupdir() {
-    readonly backup_dir="${XDG_DATA_HOME}/nvim/backup"
-    mkdir -p "${backup_dir}"
-    ok "backup-dir: ${backup_dir}"
-}
-
-skk() {
-    target="${XDG_DATA_HOME}/nvim/skk/SKK-JISYO.L"
-    rm -rf "${target}.gz" "${target}"
-    curl \
-        --create-dirs \
-        --output "${target}.gz" \
-        --location \
-        https://skk-dev.github.io/dict/SKK-JISYO.L.gz
-    gunzip "${target}.gz"
-    ok "downloading SKK-JISYO.L"
-}
-
-case ${1:-} in
-*)
-    backupdir
-    skk
-    ;;
-esac
+curl https://skk-dev.github.io/dict/SKK-JISYO.L.gz --location |
+    gunzip - >"${XDG_DATA_HOME}/nvim/skk/SKK-JISYO.L"
