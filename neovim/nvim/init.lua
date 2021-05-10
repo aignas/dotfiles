c11028514efd326e9bf3d86091de7eb23494a9af
Ignas Anikevicius <anikevicius@gmail.com>
2021-05-10 11:17:47 +0300

    neovim: reformat init.lua

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -79,15 +79,15 @@ vim.g.maplocalleader = '-'
 vim.g.vsnip_snippet_dir = os.getenv("XDG_CONFIG_HOME") .. '/nvim/vsnip'
 
 local remap = vim.api.nvim_set_keymap
-remap("n", "<Leader>b", "<CMD>Buffers<CR>", { noremap = true })
-remap("n", "<Leader>f", "<CMD>Files<CR>", { noremap = true })
-remap("n", "<Leader>e", ":e %:h/", { noremap = true })
-remap("n", "<Leader>ss", "<CMD>Gstatus<CR>", { noremap = true })
-remap("n", "<Leader>gg", ":!tdd ", { noremap = true })
-remap("n", "<Leader>cd", "<CMD>cd %:p:h<CR>", { noremap = true })
-remap("n", "<Leader>z", "<CMD>e %:h/BUILD.bazel<CR>", { noremap = true })
+remap("n", "<Leader>b", "<CMD>Buffers<CR>", {noremap = true})
+remap("n", "<Leader>f", "<CMD>Files<CR>", {noremap = true})
+remap("n", "<Leader>e", ":e %:h/", {noremap = true})
+remap("n", "<Leader>ss", "<CMD>Gstatus<CR>", {noremap = true})
+remap("n", "<Leader>gg", ":!tdd ", {noremap = true})
+remap("n", "<Leader>cd", "<CMD>lcd %:p:h<CR>", {noremap = true})
+remap("n", "<Leader>z", "<CMD>e %:h/BUILD.bazel<CR>", {noremap = true})
 
-require 'nvim-treesitter.configs'.setup {
+require'nvim-treesitter.configs'.setup {
     ensure_installed = {
       "bash",
       "c",
@@ -117,8 +117,8 @@ require 'nvim-treesitter.configs'.setup {
       "yaml",
       "zig",
     },
-    highlight = { enable = true },
-    indent = { enable = true }
+    highlight = {enable = true},
+    indent = {enable = true}
 }
 
 vim.cmd [[
@@ -142,17 +142,18 @@ autocmd BufNewFile,BufRead *.{hql,ddl} set filetype=hive expandtab
 autocmd BufWritePost init.lua PackerCompile
 ]]
 
-vim.o.grepprg=[[rg --vimgrep --no-heading -S]]
-vim.o.grepformat=[[%f:%l:%c:%m,%f:%l:%m]]
+vim.o.grepprg = [[rg --vimgrep --no-heading -S]]
+vim.o.grepformat = [[%f:%l:%c:%m,%f:%l:%m]]
 
 -- LSP
 local on_attach = function(client, bufnr)
     require('completion').on_attach()
 
-    vim.cmd [[autocmd BufWritePre *.{rs,go} lua vim.lsp.buf.formatting_sync(nil, 1000)]]
+    vim.cmd [[autocmd BufWritePre *.{rs,go,lua} lua vim.lsp.buf.formatting_sync(nil, 1000)]]
 
     local function remap(key, cmd)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', key, cmd, {noremap=true, silent=true})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', key, cmd,
+                                    {noremap = true, silent = true})
     end
     local function buf_set_option(...)
         vim.api.nvim_buf_set_option(bufnr, ...)
@@ -177,11 +178,7 @@ end
 
 local nvim_lsp = require('lspconfig')
 local servers = {'gopls', 'rust_analyzer', 'sqls', 'texlab'}
-for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {
-        on_attach = on_attach,
-    }
-end
+for _, lsp in ipairs(servers) do nvim_lsp[lsp].setup {on_attach = on_attach} end
 
 vim.g.tex_flavor = "latex"
 
@@ -202,10 +199,10 @@ vimwiki.setup({
     },
 })
 
-require 'eskk'.setup({
+require'eskk'.setup({
     start_completion_length = 2,
     directory = vim.env.XDG_DATA_HOME .. '/nvim/skk',
     select_cand_keys = 'aoeuhtns',
     show_annotation = 1,
-    kakutei_when_unique_candidate = 1,
+    kakutei_when_unique_candidate = 1
 })
