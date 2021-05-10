27373b9368b0d268c49c3dc95c9e4ab7af6cc2fb
Ignas Anikevicius <anikevicius@gmail.com>
2021-05-10 11:37:29 +0300

    bin/dotr: remove unused params and removing git push

--- lua/install.sh
+++ lua/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-luarocks install --server=https://luarocks.org/dev luaformatter
+# TODO @aignas 2021-05-10: re-enable updating when I don't need to recompile all the time.
+#luarocks install --server=https://luarocks.org/dev luaformatter
