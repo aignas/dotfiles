8e13a4c34907cf478b5063fc4409f1cfd1555050
Ignas Anikevicius <anikevicius@gmail.com>
2021-05-12 19:06:08 +0300

    remove kata and godot
    These 2 projects have been somewhat moved to the aignas/c monorepo and
    they are not needed here anymore. Since the monorepo can now publish
    debian files, this should not be needed anymore.

--- cmd/kata/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "os"
-
-func main() {
-	os.Exit(Run(os.Args[1:]))
-}
--- golang/tools.go
+++ golang/tools.go
@@ -3,8 +3,6 @@
 package tools
 
 import (
-	_ "github.com/aignas/dotfiles/cmd/godot"
-	_ "github.com/aignas/dotfiles/cmd/kata"
 	_ "github.com/git-hooks/git-hooks"
 	_ "github.com/gokcehan/lf"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
