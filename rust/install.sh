4c9ff0bba23375190e6dfa8b5dc668274bd8701a
Ignas Anikevicius <anikevicius@gmail.com>
2021-06-14 11:02:24 +0300

    rustup should work if it was just added

--- rust/install.sh
+++ rust/install.sh
@@ -4,6 +4,10 @@ set -ex
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | 
     sh -s -- -y --default-toolchain none
 
+if !type rustup >/dev/null; then
+    source "$HOME/.cargo/env"
+fi
+
 rustup toolchain install stable beta
 rustup component add clippy rustfmt \
     rust-src # needed for rust-analyzer
