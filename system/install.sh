19de3a5d1cc37c91a44462e55d7dbae8443668db
Ignas Anikevicius <anikevicius@gmail.com>
2021-06-14 07:35:03 +0300

    system: put Mac setup into a function

--- system/install.sh
+++ system/install.sh
@@ -5,39 +5,48 @@ cd "$(dirname "$0")/.."
 # shellcheck source=/dev/null
 source script/logging.sh
 
-info "brew"
-brew update
-ok "brew: update"
+_setup_mac() {
+    info "brew"
+    brew update
+    ok "brew: update"
 
-brew upgrade
-ok "brew: upgrade"
+    brew upgrade
+    ok "brew: upgrade"
 
-brew tap homebrew/cask-fonts
-brew tap jgavris/rs-git-fsmonitor https://github.com/jgavris/rs-git-fsmonitor.git
-ok "brew: tap"
+    brew tap homebrew/cask-fonts
+    brew tap jgavris/rs-git-fsmonitor https://github.com/jgavris/rs-git-fsmonitor.git
+    ok "brew: tap"
 
-debug "brew: install"
-brew install --HEAD neovim
-brew install \
-    coreutils \
-    direnv \
-    diskonaut \
-    exa \
-    fd \
-    font-hack \
-    gh \
-    golang \
-    htop \
-    jq \
-    ripgrep \
-    rs-git-fsmonitor \
-    rust-analyzer \
-    shellcheck \
-    stow \
-    texlab \
-    tig \
-    tmux \
-    tree-sitter \
-    watchman \
-    zola
-ok "brew: install"
+    debug "brew: install"
+    brew install --HEAD neovim
+    brew install \
+        coreutils \
+        direnv \
+        diskonaut \
+        exa \
+        fd \
+        font-hack \
+        gh \
+        golang \
+        htop \
+        jq \
+        npm \
+        ripgrep \
+        rs-git-fsmonitor \
+        rust-analyzer \
+        shellcheck \
+        stow \
+        texlab \
+        tig \
+        tmux \
+        tree-sitter \
+        watchexec \
+        watchman \
+        zola
+    ok "brew: install"
+}
+
+if [ "$(uname -s)" == "Darwin" ]; then
+    _setup_mac
+    return
+fi
