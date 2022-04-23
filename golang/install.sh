3d2963039ec7d6f20c4b980fee82868b5942fb5d
Ignas Anikevicius <anikevicius@gmail.com>
2022-04-23 12:36:10 +0900

    Finish zsh and golang setup

--- golang/install.sh
+++ golang/install.sh
@@ -3,10 +3,18 @@
 export GO111MODULE=on
 export GOBIN=$DOTFILES/tools
 
-if [[ -f "$(command -v go)" ]]; then
-    cd ${DOTFILES}/golang/tools
-
+if [[ -f "$(command -v goenv)" ]]; then
+    echo Installing go applications
+    goenv local 1.18.0
+    pushd ${DOTFILES}/golang/tools
     grep "_" "tools.go" |
         awk '{print $2}' |
         xargs -L 1 go install
+    popd
+
+    echo Aliasing bazelisk to bazel
+    pushd ${GOBIN}
+    rm -f bazel
+    ln -s bazelisk bazel
+    popd
 fi
