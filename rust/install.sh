5fd642db2e476faf27bce27f234d4f4b15a04d2f
Ignas Anikevicius <anikevicius@gmail.com>
2022-08-29 00:13:07 +0900

    rust: stop uninstalling fsmonitor

--- rust/install.sh
+++ rust/install.sh
@@ -2,12 +2,12 @@
 set -ex
 
 case $(./bin/dotos) in
-    "Arch Linux")
-        ;;
-    *)
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs |
-            sh -s -- -y --default-toolchain none
-        ;;
+"Arch Linux") ;;
+
+*)
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs |
+        sh -s -- -y --default-toolchain none
+    ;;
 esac
 
 source "$HOME/.cargo/env" || :
@@ -19,18 +19,16 @@ rustup default stable
 rustup update
 
 case $(./bin/dotos) in
-    Darwin|"Arch Linux")
-        ;;
-    *)
-        crates=(
-            exa
-            fd-find
-            watchexec-cli
-        )
-        echo "Installing ${crates[*]} as crates"
+Darwin | "Arch Linux") ;;
 
-        cargo install "${crates[@]}"
-        ;;
+*)
+    crates=(
+        exa
+        fd-find
+        watchexec-cli
+    )
+    echo "Installing ${crates[*]} as crates"
+
+    cargo install "${crates[@]}"
+    ;;
 esac
-
-cargo uninstall https://github.com/jgavris/rs-git-fsmonitor || :
