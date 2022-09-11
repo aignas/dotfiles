63cf1639055b65940d92883891539d3c20b4a985
Ignas Anikevicius <anikevicius@gmail.com>
2022-09-11 18:08:56 +0900

    Add wezterm as an alternative to alacritty

---
+++ wezterm/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+
+tempfile=$(mktemp) &&
+    curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/master/termwiz/data/wezterm.terminfo &&
+    tic -x -o ~/.terminfo $tempfile &&
+    rm $tempfile
---
+++ wezterm/wezterm/wezterm.lua
@@ -0,0 +1,5 @@
+local wezterm = require 'wezterm'
+return {
+    color_scheme = 'Default Dark (base16)',
+    font = wezterm.font 'Hack',
+}
