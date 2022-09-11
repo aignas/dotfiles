172ed92fbd76d9a135fc1cc56a36801705eca9de
Ignas Anikevicius <anikevicius@gmail.com>
2022-09-11 18:17:44 +0900

    wezterm: Connect to the unix domain by default

--- wezterm/wezterm/wezterm.lua
+++ wezterm/wezterm/wezterm.lua
@@ -2,4 +2,12 @@ local wezterm = require 'wezterm'
 return {
     color_scheme = 'Default Dark (base16)',
     font = wezterm.font 'Hack',
+
+    unix_domains = {
+        {
+            name = 'unix',
+        },
+    },
+
+    default_gui_startup_args = { 'connect', 'unix' },
 }
