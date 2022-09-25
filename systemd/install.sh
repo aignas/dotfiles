9aefd22008738d70f688fa44a42e956ebd410d7e
Ignas Anikevicius <anikevicius@gmail.com>
2022-09-25 14:29:18 +0900

    systemd: Add notes

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # A small hack to install customizations for some of the user systemd services.
+# It seems that if I put the symlinks to this dir, systemd won't detect changes
+# to the service files.
 
 dirs=(
     spotifyd.service.d
