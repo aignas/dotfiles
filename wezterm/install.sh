e4d4126137ac91f101770dd8df5bffcf1d6af80e
Ignas Anikevicius <anikevicius@gmail.com>
2022-09-25 10:48:51 +0900

    Remove wezterm

--- wezterm/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-tempfile=$(mktemp) &&
-    curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/master/termwiz/data/wezterm.terminfo &&
-    tic -x -o ~/.terminfo $tempfile &&
-    rm $tempfile
