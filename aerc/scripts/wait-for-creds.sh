99367183c06a9584747ceb90827e69b978e63cbe
Ignas Anikevicius <ignas.anikevicius@woven-planet.global>
2023-05-08 09:19:41 +0900

    feat(aerc): support reading credentials from a different file

--- aerc/scripts/wait-for-creds.sh
+++ aerc/scripts/wait-for-creds.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ "$1" == "local" && -f $2 ]]; then
+    pass=$(cat "$2")
+    echo -n "$pass"
+    exit 0
+fi
 
 secret-tool lookup "$1" "$2"
 # wait until the password is available
