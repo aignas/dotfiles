72b862066a40dc91d2555956a0616d67f8e8bf7e
Ignas Anikevicius <anikevicius@gmail.com>
2023-09-18 11:39:45 +0900

    aerc: improve cred waiting script

--- aerc/scripts/wait-for-creds.sh
+++ aerc/scripts/wait-for-creds.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [[ "$1" == "local" && -f $2 ]]; then
+if [[ $1 == "local" && -f $2 ]]; then
     cat "$2"
     exit 0
 fi
 
-secret-tool lookup "$1" "$2"
 # wait until the password is available
-while [ $? != 0 ]; do
-    secret-tool lookup "$1" "$2"
+echo "Waiting for credentials for '$*'..." >/tmp/wait-for-creds.log
+while ! secret-tool lookup "$1" "$2"; do
+    echo "Waiting for credentials for '$*'..." >>/tmp/wait-for-creds.log
+    sleep 5
 done
