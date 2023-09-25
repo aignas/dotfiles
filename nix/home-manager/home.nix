70b895b5684910678ec7cab7fd010bf15edf5be1
Ignas Anikevicius <240938+aignas@users.noreply.github.com>
2023-09-25 11:24:28 +0900

    nix: remove the home variables

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -6,34 +6,22 @@
   home.username = "aignas";
   home.homeDirectory = "/home/aignas";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "23.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    pkgs.aerc
+    pkgs.lf
+    pkgs.newsboat
+    pkgs.eza
   ];
 
   # Manage dotfiles
   home.file = {
+    ".local/bin" = {
+        source = ../../bin;
+        recursive = true;
+    };
+
     ".config/aerc/aerc.conf".source = ../../aerc/aerc.conf;
     ".config/aerc/binds.conf".source = ../../aerc/binds.conf;
     ".config/aerc/scripts" = {
@@ -99,19 +87,8 @@
     ".zshrc".source = ../../zsh/dot-zshrc;
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/aignas/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
-  # home.sessionVariables = {
-  #   # EDITOR = "e";
-  # };
+  home.sessionVariables = {
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
