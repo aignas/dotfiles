b4dd72cc20255a4f8ecd0e8a8426fe750fc05965
Ignas Anikevicius <240938+aignas@users.noreply.github.com>
2023-10-16 21:12:02 +0900

    nix: add unstable channel to install.sh

--- nix/install.sh
+++ nix/install.sh
@@ -5,6 +5,7 @@ type nix-channel >/dev/null || exit 0
 nix-channel \
     --add https://github.com/nix-community/home-manager/archive/master.tar.gz \
     home-manager
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable
 nix-channel --update
 
 nix-shell '<home-manager>' -A install
