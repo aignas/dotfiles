e16d9d6479bcc941a81de462477f716d5516a97e
Ignas Anikevicius <240938+aignas@users.noreply.github.com>
2023-11-04 09:19:01 +0900

    feat(nix): improve initial nix setup

--- nix/install.sh
+++ nix/install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+set -euo pipefail
 
 type nix-channel >/dev/null || exit 0
 
-nix-channel \
-    --add https://github.com/nix-community/home-manager/archive/master.tar.gz \
-    home-manager
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+if ! type home-manager >/dev/null; then
+    nix-channel \
+        --add https://github.com/nix-community/home-manager/archive/master.tar.gz \
+        home-manager
+    nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+fi
 nix-channel --update
+# We need to only do an initial install as home-manager can manage itself
+type home-manager >/dev/null || nix-shell '<home-manager>' -A install
 
-nix-shell '<home-manager>' -A install
 mkdir -p ~/.config/home-manager
 pushd ~/.config/home-manager
 rm home.nix
-ln -s ~/.dotfiles/nix/home.nix
+ln -s ~/.dotfiles/nix/home.nix .
 popd
